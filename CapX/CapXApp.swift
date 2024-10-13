da7f8af63a81ee4aa41917bce409191fe5c76e5a
pravalgautam <71179696+pravalgautam@users.noreply.github.com>
2024-10-14 00:44:24 +0530

    Initial Commit

---
+++ CapX/CapXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapXApp.swift
+//  CapX
+//
+//  Created by Praval Gautam on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CapXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
